15922ba1fef0075cc70bdabd7678999fbb9122e9
Jamie White <jamie@jgwhite.co.uk>
2015-09-22 06:07:16 +0100

    Remove old helpers

--- app/helpers/either-or.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function eitherOr([a, b]/*, hash*/) {
-  return a || b;
-}
-
-export default Ember.Helper.helper(eitherOr);
--- app/helpers/is-eq.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function isEq([a, b]) {
-  return a === b;
-}
-
-export default Ember.Helper.helper(isEq);
